aa2a572882d5ce0f34f2fccff77f37eb6b5d2a66
montanafowler@yahoo.com <montanafowler@yahoo.com>
2020-05-29 10:15:55 -0700

    pushing unity project that has a compute shader in a render texture on a game object but not yet on an object in the scene

---
+++ SlimeArt/Assets/ComputeHookup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputeHookup : MonoBehaviour
+{
+
+    public ComputeShader compute;
+    public RenderTexture result;
+    public Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int kernel = compute.FindKernel("CSMain");
+        result = new RenderTexture(512, 512, 24);
+        result.enableRandomWrite = true;
+        result.Create();
+
+        compute.SetTexture(kernel, "Result", result);
+        compute.Dispatch(kernel, 512 / 8, 512 / 8, 1);
+        mat.mainTexture = result;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
