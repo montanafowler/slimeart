7b0a6a060eb8bef8c92c164380ed2f533ca2c66a
montanafowler@yahoo.com <montanafowler@yahoo.com>
2020-06-26 16:41:08 -0700

    trying to commit new project

--- SlimeArt/Assets/ComputeHookup.cs
+++ SlimeArt/Assets/ComputeHookup.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ComputeHookup : MonoBehaviour
-{
+{ 
     public ComputeShader compute;
     public ComputeShader decay;
     public ComputeBuffer initialGalaxyPositions;
@@ -17,8 +17,7 @@ public class ComputeHookup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        //hello world
+        Debug.Log("TESTING SAVING ISSUE");
 
         // kernel is the compute shader (initial spark)
         // result will have the thing to blur in it
@@ -26,12 +25,12 @@ public class ComputeHookup : MonoBehaviour
         int pixelHeight = 512;
         int pixelWidth = (int)(Camera.main.aspect * pixelHeight);
         Debug.Log(pixelWidth);
-
+  
         result = new RenderTexture(pixelWidth, pixelHeight, 32);
         result.enableRandomWrite = true;
         result.Create();
 
-        float[] testArray = { 10.0f, 10.0f, 0.0f };
+        float[]testArray = { 10.0f, 10.0f, 0.0f };
         initialGalaxyPositions = new ComputeBuffer(testArray.Length, sizeof(float));
         initialGalaxyPositions.SetData(testArray);
         compute.SetBuffer(computeKernel, "buffer", initialGalaxyPositions);
@@ -43,7 +42,7 @@ public class ComputeHookup : MonoBehaviour
 
         //decay kernel
         int decayKernel = decay.FindKernel("CSMain");
-
+        
         // deposit_in
         //deposit_in = new RenderTexture(512, 512, 32);
         //deposit_in.enableRandomWrite = true;
@@ -55,18 +54,18 @@ public class ComputeHookup : MonoBehaviour
         deposit_out.Create();
 
         // tex_trace is keeping track of the trace
-        // tex_trace = new RenderTexture(512, 512, 32);
-        //  tex_trace.enableRandomWrite = true;
-        //  tex_trace.Create();
+       // tex_trace = new RenderTexture(512, 512, 32);
+      //  tex_trace.enableRandomWrite = true;
+      //  tex_trace.Create();
 
         // send squares texture in as deposit_in
         decay.SetTexture(decayKernel, "deposit_in", result);
         decay.SetTexture(decayKernel, "deposit_out", deposit_out);
-        // decay.SetTexture(decayKernel, "tex_trace", tex_trace);
+       // decay.SetTexture(decayKernel, "tex_trace", tex_trace);
         decay.Dispatch(decayKernel, 512 / 8, 512 / 8, 1);
 
         float h = Camera.main.pixelWidth;
-        // Debug.Log(h);
+       // Debug.Log(h);
 
         mat.mainTexture = deposit_out;
     }
